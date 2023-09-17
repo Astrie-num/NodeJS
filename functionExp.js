effbd80a29743c550521cc60ad9528196c6b2b54
Astrie <uwasterie07@gmail.com>
2023-09-17 15:32:44 +0200

    Initial commit

---
+++ functionExp.js
@@ -0,0 +1,36 @@
+// Function expressions
+
+// Normal function statement
+
+
+function sayHi(){
+    console.log("Hi");
+}
+sayHi();
+
+// Function expression
+
+var sayBye = function(){
+    console.log("Bye");
+}
+
+sayBye();
+
+
+//  Using both function statement and function expression in the same script
+
+
+//Normal function statement
+function add(a,b){
+    return (a + b);
+}
+console.log(add(2,3));
+
+
+//Expression
+
+var subtract = function(a,b){
+    return (a - b);
+}
+
+console.log(subtract(4,3));
---
+++ globalVar.js
@@ -0,0 +1,23 @@
+ // The global object
+
+
+ 
+// var time = 0;
+// var timer = setInterval(function(){
+//     time += 1;
+//     console.log(time + "Seconds passed!")
+//     if(time < 5){
+//         clearInterval(timer)
+//     }
+
+// },2000)
+
+// console.log("I am by the names of Astrie!!")
+// setTimeout(function(){
+//     console.log("Three secondas have passed!")
+// },3000)
+
+console.log(__dirname)
+console.log(__filename)
+console.log(process)
+console.log(module)
